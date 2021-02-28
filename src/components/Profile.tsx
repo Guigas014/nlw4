0e8744edd0f0bcfdb3cf1ee202ad30d3f55aa900
Guilherme <guilherme-allves@hotmail.com>
2021-02-28 01:08:02 -0300

    moveit-next created

---
+++ src/components/Profile.tsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
+import styles from '../styles/components/Profile.module.css';
+
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext)  
+
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/chucrutao.png" alt="" />
+      <div>
+        <strong>Guilherme</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
